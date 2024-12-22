0c29fdeec1bc3e2ab7a6db47bf9af7b95b3ee9d6
notKamui <teillard.jimmy@outlook.fr>
2024-12-22 19:18:59 +0100

    stats wip

--- app/routes/_authed/time/stats.tsx
+++ app/routes/_authed/time/stats.tsx
@@ -13,6 +13,16 @@ import {
   YAxis,
 } from 'recharts'
 
+const DAYS = {
+  1: 'Monday',
+  2: 'Tuesday',
+  3: 'Wednesday',
+  4: 'Thursday',
+  5: 'Friday',
+  6: 'Saturday',
+  7: 'Sunday',
+} as const
+
 export const Route = createFileRoute('/_authed/time/stats')({
   loader: async () => {
     const time = Time.now()
@@ -35,28 +45,17 @@ export const Route = createFileRoute('/_authed/time/stats')({
 function RouteComponent() {
   const { stats } = Route.useLoaderData()
 
-  console.log(stats)
-
-const fullRange = [1, 2, 3, 4, 5, 6, 7];
-const data = fullRange.map(dayOrMonth => {
-  const entry = stats.find(stat => stat.dayOrMonth === dayOrMonth);
-  return {
-    day: {
-      1: 'Monday',
-      2: 'Tuesday',
-      3: 'Wednesday',
-      4: 'Thursday',
-      5: 'Friday',
-      6: 'Saturday',
-      7: 'Sunday'
-    }[dayOrMonth],
-    total: entry ? entry.total : 0,
-  };
-});
-
+  const fullRange = Object.keys(DAYS).map(Number)
+  const data = fullRange.map((dayOrMonth) => {
+    const entry = stats.find((stat) => stat.dayOrMonth === dayOrMonth)
+    return {
+      day: DAYS[dayOrMonth],
+      total: entry ? entry.total : 0,
+    }
+  })
 
   return (
-    <div className='size-full'>
+    <div className="size-full">
       <ResponsiveContainer width="100%" height="100%">
         <LineChart
           width={500}
@@ -71,10 +70,20 @@ const data = fullRange.map(dayOrMonth => {
         >
           <CartesianGrid strokeDasharray="3 3" />
           <XAxis dataKey="day" />
-          <YAxis dataKey="total" tickFormatter={(total) => Time.formatTime(total * 1000)} />
-          <Tooltip formatter={(total: number) => Time.formatTime(total * 1000)} />
+          <YAxis
+            dataKey="total"
+            tickFormatter={(total) => Time.formatTime(total * 1000)}
+          />
+          <Tooltip
+            formatter={(total: number) => Time.formatTime(total * 1000)}
+          />
           <Legend />
-          <Line type="monotone" dataKey="total" stroke="#8884d8" activeDot={{ r: 8 }} />
+          <Line
+            type="monotone"
+            dataKey="total"
+            stroke="#8884d8"
+            activeDot={{ r: 8 }}
+          />
         </LineChart>
       </ResponsiveContainer>
     </div>
