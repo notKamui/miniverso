2a32e266143d775c46fae35d3782a466340e8b1c
notKamui <46132319+notKamui@users.noreply.github.com>
2025-11-22 18:34:13 +0100

    feat: add time entry relations and update schema for user associations

--- src/server/db/time.schema.ts
+++ src/server/db/time.schema.ts
@@ -1,17 +1,32 @@
-import type { InferSelectModel } from 'drizzle-orm'
-import { pgTable, text, uuid } from 'drizzle-orm/pg-core'
+import { type InferSelectModel, relations } from 'drizzle-orm'
+import { index, pgTable, text, uuid } from 'drizzle-orm/pg-core'
 import { Time } from '@/lib/utils/time'
 import { user } from '@/server/db/auth.schema'
 
-export const timeEntriesTable = pgTable('time_entry', {
-  id: uuid('id').primaryKey().defaultRandom(),
-  userId: text('user_id')
-    .notNull()
-    .references(() => user.id, { onDelete: 'cascade' }),
-  startedAt: Time.column('started_at')
-    .$defaultFn(() => /* @__PURE__ */ Time.now())
-    .notNull(),
-  endedAt: Time.column('ended_at'),
-  description: text('description'),
-})
+export const timeEntriesTable = pgTable(
+  'time_entry',
+  {
+    id: uuid('id').primaryKey().defaultRandom(),
+    userId: text('user_id')
+      .notNull()
+      .references(() => user.id, { onDelete: 'cascade' }),
+    startedAt: Time.column('started_at')
+      .$defaultFn(() => /* @__PURE__ */ Time.now())
+      .notNull(),
+    endedAt: Time.column('ended_at'),
+    description: text('description'),
+  },
+  (table) => [index('time_entry_userId_idx').on(table.userId)],
+)
 export type TimeEntry = InferSelectModel<typeof timeEntriesTable>
+
+export const userRelations = relations(user, ({ many }) => ({
+  timeEntries: many(timeEntriesTable),
+}))
+
+export const timeEntryRelations = relations(timeEntriesTable, ({ one }) => ({
+  user: one(user, {
+    fields: [timeEntriesTable.userId],
+    references: [user.id],
+  }),
+}))
