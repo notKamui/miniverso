d0fe77eafede6cb5313b996eb8184a0eafc6d541
notKamui <teillard.jimmy@outlook.fr>
2026-01-17 14:58:18 +0100

    chore: update Vite to version 8.0.0-beta.8 and remove vite-tsconfig-paths plugin from configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import { devtools } from '@tanstack/devtools-vite'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
-import viteTsConfigPaths from 'vite-tsconfig-paths'
 import pkg from './package.json'
 
 const manualChunks = {
@@ -22,15 +21,12 @@ const manualChunks = {
 } as const
 
 const config = defineConfig({
-  plugins: [
-    devtools(),
-    viteTsConfigPaths({ projects: ['./tsconfig.json'] }),
-    tailwindcss(),
-    tanstackStart(),
-    react(),
-  ],
+  plugins: [devtools(), tailwindcss(), tanstackStart(), react()],
   define: {
     APP_VERSION: JSON.stringify(pkg.version),
+  },
+  resolve: {
+    tsconfigPaths: true,
   },
   optimizeDeps: {
     // TODO: remove this once better-auth fixes its virtual imports
